fdfc1e59542efe1b592653cb3a6fad1a1a3523dd
wangxt <wangxt@ckt.cn>
2022-03-24 18:24:07 +0800

    init

---
+++ src/main/java/com/wangxt/practise/WxtPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.wangxt.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxtPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxtPractiseApplication.class, args);
+	}
+
+}
